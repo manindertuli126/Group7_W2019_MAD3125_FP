7b46b7d767e5441e70dd070f098f1e1161ebbf88
Maninder Singh Tuli <maninder.tuli126@gmail.com>
2019-04-04 18:40:05 -0400

    Initial commit and added sign up, login and welcome activity.

---
+++ app/src/main/java/com/example/group7_w2019_mad3125_fp/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.group7_w2019_mad3125_fp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        setTitle("Sign Up");
+    }
+}
